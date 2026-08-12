575486a69f8d3aca6e5a9dbede9e54ed32937606
universalcode115-create <universalcode115@gmail.com>
2026-08-12 11:56:05 +0530

    Delete const.ts

--- const.ts
+++
@@ -1,4 +0,0 @@
-export const COOKIE_NAME = "gullyexpert_session";
-export const SESSION_MAX_AGE_MS = 1000 * 60 * 60 * 24 * 30;
-export const UNAUTHED_ERR_MSG = "Please log in to continue";
-export const NOT_ADMIN_ERR_MSG = "You do not have required permission";
